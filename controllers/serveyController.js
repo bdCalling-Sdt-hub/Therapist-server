2e25cddc632656970ca7c0bf856ffd4f333e939f
towhidul1999 <towhidulsislam@gmail.com>
2024-03-16 15:16:47 +0600

    without answer api

--- controllers/serveyController.js
+++ controllers/serveyController.js
@@ -39,7 +39,7 @@ const getServey = async (req, res) => {
     } catch (error) {
         res.status(500).json(Response({ error: error }));
     }
-}
+};
 
 const getServeyAnswer = async (req, res) => {
     try {
@@ -75,9 +75,60 @@ const getServeyAnswer = async (req, res) => {
     }
 };
 
+const getServeyWithoutAnswer = async (req, res) => {
+    try {
+        let surveyQuestionWithAnswer = [];
+
+        // Retrieve all survey answers
+        const surveyAnswers = await SurveyAnswer.find();
+
+        // Extract questionIds from survey answers
+        const questionIds = surveyAnswers.map(answer => answer.questionId);
+
+        // Retrieve questions based on the extracted questionIds
+        const questions = await Survey.find({ _id: { $in: questionIds } });
+
+        // Associate each survey answer with its corresponding question
+        surveyQuestionWithAnswer = surveyAnswers.map(answer => {
+            const question = questions.find(q => q._id.toString() === answer.questionId.toString());
+            return {
+                _id: answer._id,
+                questionId: answer.questionId,
+                question: question, // Adding question to the object
+                answers: answer.answers,
+                createdAt: answer.createdAt,
+                updatedAt: answer.updatedAt,
+                __v: answer.__v
+            };
+        });
+
+        res.status(200).json({ data: surveyQuestionWithAnswer });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+const serveyAnswer = async (req, res) => {
+    try {
+        const { questionId, answers, correctAnswers } = req.body;
+
+        const createAnswer = await SurveyAnswer.create({
+            answers: answers,
+            correctAnswers: correctAnswers,
+            questionId: questionId,
+        });
+
+        res.status(201).json(Response({ data: createAnswer }));
+    } catch (error) {
+        res.status(500).json(Response({ error: error }));
+    }
+}
+
 
 module.exports = {
     createServey,
     getServey,
-    getServeyAnswer
+    getServeyAnswer,
+    getServeyWithoutAnswer
 }
--- routes/serveyRouter.js
+++ routes/serveyRouter.js
@@ -4,12 +4,13 @@ const router = express.Router();
 //import controllers
 const upload = require('../middlewares.js/fileUpload');
 const { isValidUser } = require('../middlewares.js/auth');
-const { createServey, getServey, getServeyAnswer } = require('../controllers/serveyController');
+const { createServey, getServey, getServeyAnswer, getServeyWithoutAnswer } = require('../controllers/serveyController');
 
 
 // routes
 router.post('/add', createServey);
 router.get('/all', getServey);
 router.get("/answers", getServeyAnswer);
+router.get("/provide-answer", getServeyWithoutAnswer);
 
 module.exports = router;
